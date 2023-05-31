09c6f9cf10bb79a822ab0578cdb0051fde8c07b1
Chirag Suthar <chiragsuthar236@gmail.com>
2023-05-31 16:35:55 +0530

    done some additional changes

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('create-room/',views.createRoom,name='create-room'),
     path('update-room/<str:pk>',views.updateRoom,name='update-room'),
     path('delete-room/<str:pk>',views.deleteRoom,name='delete-room'),
+    path('delete-message/<str:pk>',views.deleteMessage,name='delete-message'),
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
-    path('register/', views.registerPage, name='register')
+    path('register/', views.registerPage, name='register'),
+    path('profile/<str:pk>', views.userProfile, name='user-profile')
 ]
